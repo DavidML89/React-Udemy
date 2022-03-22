04f9d1297f760fde448653c96bb76548d6057659
David Morio <david.morio.lesavourey@gmail.com>
2022-03-22 18:21:12 +0100

    copied

--- 02-introducing-jsx/code/03_JSX_Conditionals/index.js
+++ 02-introducing-jsx/code/03_JSX_Conditionals/index.js
@@ -4,9 +4,11 @@ function getNumber() {
 
 class NumPicker extends React.Component {
   render() {
+    const num = getNumber();
     return (
       <div>
-        <h1>Your number is {getNumber()}</h1>
+        <h1>Your number is {num}</h1>
+        <p>{num === 7 ? 'You win' : 'Looooooser'}</p>
       </div>
     )
   }
