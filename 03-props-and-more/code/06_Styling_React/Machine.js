0a9e10b38c0d9720c9a2d586f2ff940358ca2c68
David Morio <david.morio.lesavourey@gmail.com>
2022-03-22 19:37:03 +0100

    Excercise done and refactored

--- 03-props-and-more/code/06_Styling_React/Machine.js
+++ 03-props-and-more/code/06_Styling_React/Machine.js
@@ -1,16 +1,15 @@
 class Machine extends React.Component {
-	render() {
-		const { s1, s2, s3 } = this.props;
-		const winner = s1 === s2 && s2 === s3;
-		const colors = { fontSize: '50px', backgroundColor: 'purple' };
+  render() {
+    const {s1, s2, s3} = this.props;
+    const winner = ( s1 === s2 ) && ( s2 === s3 );
+    const colors = {fontSize: '50px', backgroundColor: 'blue'};
 
-		return (
-			<div className="Machine">
-				<p style={colors}>
-					{s1} {s2} {s3}
-				</p>
-				<p className={winner ? 'win' : 'lose'}>{winner ? 'Winner!' : 'Loser!'}</p>
-			</div>
-		);
-	}
+    return (
+      <div className='Machine'>
+        <h2>Machine {this.props.machineNumber}</h2>
+        <p style={colors}>{s1}{s2}{s3}</p>
+        <p className={ winner ? 'win' : 'lose' }>{winner ? 'Yeah' : 'Booohhhhh'}</p>
+      </div>
+    )
+  }
 }
