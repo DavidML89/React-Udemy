8df7c45ebc1a15edf99c55eb0937ea83c9a07e51
David Morio <david.morio.lesavourey@gmail.com>
2022-03-23 15:36:02 +0100

    Random number method corrected

--- 06-react-state/code/random-number/src/Random.js
+++ 06-react-state/code/random-number/src/Random.js
@@ -4,13 +4,19 @@ class Random extends Component {
   constructor(props) {
     super(props);
     this.state = {number: 1 };
+    // this.genRandomNum = this.genRandomNum.bind(this);
+  }
+
+  genRandomNum = (e) => {
+    let randNum = Math.floor(Math.random() * 10) + 1;
+    this.setState ( { number: randNum } )
   }
 
   render() {
     return (
       <div className='Random'>
         <h1>Number is : {this.state.number}</h1>
-        <button>Random number</button>
+        <button onClick={this.genRandomNum}>Random number</button>
       </div>
     )
   }
