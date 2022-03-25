84a1aade3b42fa2f3206a172543b47a536bb0d95
David Morio <david.morio.lesavourey@gmail.com>
2022-03-25 20:32:30 +0100

    BoxContainer component corrected

---
+++ 09-state-exercises/color-boxes/src/Box.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+  
+
+  render() {
+    return (
+      <div className='Box'>
+        Hello from the box
+      </div>
+    )
+  }
+}
+
+export default Box;
--- 09-state-exercises/color-boxes/src/BoxContainer.js
+++ 09-state-exercises/color-boxes/src/BoxContainer.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-// import Box from './Box';
+import Box from './Box';
 
 class BoxContainer extends Component {
+  static defaultProps = {
+    colors: [ "purple", "magenta", "violet", "pink" ],
+    numBoxes: 18
+  }
+
   render() {
+    const boxes = Array.from( { length: this.props.numBoxes }).map(() => (
+      <Box colors={this.propös.colors} />
+    ));
     return (
       <div className='BoxContainer'>
-        Hello from the box container
+        {boxes}
       </div>
-    )
-  }
+    );
+  };
 }
 
 export default BoxContainer;
