945ad2622863e94037c4c0079cfa6431e1bed1e1
David Morio <david.morio.lesavourey@gmail.com>
2022-03-25 20:17:17 +0100

    BoxContainer component created

---
+++ 09-state-exercises/color-boxes/src/BoxContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import Box from './Box';
+
+class BoxContainer extends Component {
+  render() {
+    return (
+      <div className='BoxContainer'>
+        Hello from the box container
+      </div>
+    )
+  }
+}
+
+export default BoxContainer;
