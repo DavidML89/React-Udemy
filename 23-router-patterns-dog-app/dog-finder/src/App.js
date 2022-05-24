8ac83006d75e5a83fa4e5b7b79e37f8c7050df35
David Morio <david.morio.lesavourey@gmail.com>
2022-05-24 16:37:19 +0200

    Basic component navbar created. without css

--- 23-router-patterns-dog-app/dog-finder/src/App.js
+++ 23-router-patterns-dog-app/dog-finder/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
+import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: './images/whiskey.jpg',
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: './images/hazel.jpg',
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: './images/tubby.jpg',
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
+  render () {
+    return (
+      <div className="App">
+        <Navbar dogs={this.props.dogs} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ 23-router-patterns-dog-app/dog-finder/src/Navbar.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    const names = this.props.dogs.map(d => (
+      <li className="navbar-item">
+        <NavLink exact to={`/dogs/${d.name}`}>
+          {d.name}
+        </NavLink>
+      </li>
+    ));
+    return <div>{names}</div>
+  }
+}
+
+export default Navbar;
