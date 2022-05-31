0272b2880d58c7f302d43dc45c8496b4501bf475
David Morio <david.morio.lesavourey@gmail.com>
2022-05-31 12:27:22 +0200

    Context app project initiated

---
+++ 27-react-context-intro/context-demo-app-add-initial-components/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeProvider } from "./Contexts/ThemeContext"
+
+class App extends Component {
+  render() {
+    return (
+      <>
+       <ThemeProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+       </ThemeProvider>
+      </>
+    );
+  }
+}
+
+export default App;
