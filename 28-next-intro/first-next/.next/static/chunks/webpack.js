f7a9ad871dcb82bedcbe1821fe2a3ae1bfa4de7e
David Morio <david.morio.lesavourey@gmail.com>
2022-06-08 17:59:25 +0200

    New server with express for custom routes created

--- 28-next-intro/first-next/.next/static/chunks/webpack.js
+++ 28-next-intro/first-next/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c0acda8fc0ad6888"; }
+/******/ 		__webpack_require__.h = function() { return "d30f5b8c9a60e571"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
